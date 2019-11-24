2957b4b70753e572b1dd2ad457b5eb45d3713803
isaac-ireland <ire10001@byui.edu>
2019-11-23 21:14:07 -0700

    JUnit tests updated

--- familiarity review/JUnit/JUnitTestAssertThatAssertions.java
+++ familiarity review/JUnit/JUnitTestAssertThatAssertions.java
@@ -1,6 +1,9 @@
 package com.areyes1.junitassertrue.sample;
  
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +11,8 @@ import org.junit.Test;
 public class JUnitTestAssertThatAssertions {
      
     int totalNumberOfApplicants = 0;
-     
+    int NumberOfApplicants;
+
     @Before
     public void setData(){
         this.totalNumberOfApplicants = 9;
@@ -18,7 +22,31 @@ public class JUnitTestAssertThatAssertions {
     public void testAssertThatEqual() {
         assertThat("123",is("123"));
     }
-     
+
+    // added assert is not null
+    @Test 
+    public void testnotNull() {
+        assertNotNull(totalNumberOfApplicants);
+
+    }
+
+
+    // added assert is not null
+    @Test 
+    public void testNull(){
+    // I'm testing undefined veriables are null
+        assertNull(NumberOfApplicants);
+
+    }
+    
+    // added assert same
+    @Test 
+    public void testNull(){
+    // I'm testing if I can complare these two
+        NumberOfApplicants = totalNumberOfApplicants;
+        assertSame(NumberOfApplicants, totalNumberOfApplicants);
+    } 
+
     @Test
     public void testAssertThatNotEqual() {
         assertThat(totalNumberOfApplicants,is(123));
