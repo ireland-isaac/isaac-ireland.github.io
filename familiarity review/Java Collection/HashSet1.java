fad75561ace64923c75693d60b75712ea39ff317
isaac-ireland <ire10001@byui.edu>
2019-11-23 20:56:12 -0700

    update HashSet1 for functionality

--- familiarity review/Java Collection/HashSet1.java
+++ familiarity review/Java Collection/HashSet1.java
@@ -1,4 +1,7 @@
+// from javatpoint.com 
+
 import java.util.*;  
+
 class HashSet1{  
  public static void main(String args[]){  
   //Creating HashSet and adding elements  
@@ -7,7 +10,12 @@ class HashSet1{
            set.add("Two");    
            set.add("Three");   
            set.add("Four");  
-           set.add("Five");  
+           set.add("Five");
+           // example of how set works with duplicates
+           set.add("Five");
+           // example of adding and removing
+           set.add("onne");
+           set.remove("onne");
            Iterator<String> i=set.iterator();  
            while(i.hasNext())  
            {  
